6bbd4ee8bce17765e84731138bc92cacaff15fee
Precious Humphrey <phumphrey23@gmail.com>
2018-04-19 03:18:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,3 @@
 def reverse_each_word(word)
-  arr_split = word.split(" ")
-  arr_rev = arr_split.each {|letters| letters.reverse}
-  return arr_rev.join(" ")
+  word.split(" ").collect {|letters| letters.reverse}.join(" ")
 end
-
-
-#def reverse_each_word(word)
-#  word.split(" ").collect {|letters| letters.reverse}.join(" ")
-#end
